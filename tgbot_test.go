5ffdf75e8918fd3ffcdbe37d241a54cb92e75362
zhongqi <imzhongqi@gmail.com>
2022-05-15 00:03:14 +0800

    add example test.

--- tgbot_test.go
+++ tgbot_test.go
@@ -1,6 +1,9 @@
 package tgbot_test
 
 import (
+	"log"
+	"time"
+
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/imzhongqi/tgbot"
 )
@@ -11,7 +14,26 @@ func ExampleNewBot() {
 		panic(err)
 	}
 
-	bot := tgbot.NewBot(api)
+	bot := tgbot.NewBot(api,
+		tgbot.WithTimeout(2*time.Second),
+
+		tgbot.WithUpdatesHandler(func(ctx *tgbot.Context) {
+			err := ctx.ReplyText(ctx.Update().Message.Text, func(c *tgbotapi.MessageConfig) {
+				c.ReplyToMessageID = ctx.Message().MessageID
+			})
+			if err != nil {
+				log.Printf("reply text error: %s", err)
+			}
+		}),
+
+		tgbot.WithUndefinedCmdHandler(func(ctx *tgbot.Context) error {
+			return ctx.ReplyMarkdown("*unknown command*", tgbot.WithEnableWebPagePreview())
+		}),
+
+		tgbot.WithErrorHandler(func(err error) {
+			log.Println(err)
+		}),
+	)
 	bot.AddCommand(&tgbot.Command{
 		Name:        "ping",
 		Description: "ping the bot",
@@ -22,7 +44,4 @@ func ExampleNewBot() {
 	if err := bot.Run(); err != nil {
 		panic(err)
 	}
-
-	// output:
-
 }
