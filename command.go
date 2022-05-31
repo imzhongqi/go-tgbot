43cab5af68369ea27f10eeda4763537ff001e7c5
zhongqi <imzhongqi@gmail.com>
2022-05-31 11:15:16 +0800

    refactor: add scope type constant.

--- command.go
+++ command.go
@@ -6,6 +6,16 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
+const (
+	ScopeTypeDefault               = "default"
+	ScopeTypeAllPrivateChats       = "all_private_chats"
+	ScopeTypeAllGroupChats         = "all_group_chats"
+	ScopeTypeAllChatAdministrators = "all_chat_administrators"
+	ScopeTypeChat                  = "all_chat_administrators"
+	ScopeTypeChatAdministrators    = "chat_administrators"
+	ScopeTypeChatMember            = "chat_member"
+)
+
 // CommandScope represent a telegram command scope.
 type CommandScope struct {
 	invalid bool
@@ -40,32 +50,32 @@ func (c CommandScope) toScope() tgbotapi.BotCommandScope {
 
 // NewCommandScopeDefault represents the default scope of bot commands.
 func NewCommandScopeDefault() CommandScope {
-	return CommandScope{Type: "default"}
+	return CommandScope{Type: ScopeTypeDefault}
 }
 
 // NewCommandScopeAllPrivateChats represents the scope of bot commands,
 // covering all private chats.
 func NewCommandScopeAllPrivateChats() CommandScope {
-	return CommandScope{Type: "all_private_chats"}
+	return CommandScope{Type: ScopeTypeAllPrivateChats}
 }
 
 // NewCommandScopeAllGroupChats represents the scope of bot commands,
 // covering all group and supergroup chats.
 func NewCommandScopeAllGroupChats() CommandScope {
-	return CommandScope{Type: "all_group_chats"}
+	return CommandScope{Type: ScopeTypeAllGroupChats}
 }
 
 // NewCommandScopeAllChatAdministrators represents the scope of bot commands,
 // covering all group and supergroup chat administrators.
 func NewCommandScopeAllChatAdministrators() CommandScope {
-	return CommandScope{Type: "all_chat_administrators"}
+	return CommandScope{Type: ScopeTypeAllChatAdministrators}
 }
 
 // NewCommandScopeChat represents the scope of bot commands, covering a
 // specific chat.
 func NewCommandScopeChat(chatID int64) CommandScope {
 	return CommandScope{
-		Type:   "chat",
+		Type:   ScopeTypeChat,
 		ChatID: chatID,
 	}
 }
@@ -74,7 +84,7 @@ func NewCommandScopeChat(chatID int64) CommandScope {
 // covering all administrators of a specific group or supergroup chat.
 func NewCommandScopeChatAdministrators(chatID int64) CommandScope {
 	return CommandScope{
-		Type:   "chat_administrators",
+		Type:   ScopeTypeChatAdministrators,
 		ChatID: chatID,
 	}
 }
@@ -83,7 +93,7 @@ func NewCommandScopeChatAdministrators(chatID int64) CommandScope {
 // specific member of a group or supergroup chat.
 func NewCommandScopeChatMember(chatID, userID int64) CommandScope {
 	return CommandScope{
-		Type:   "chat_member",
+		Type:   ScopeTypeChatMember,
 		ChatID: chatID,
 		UserID: userID,
 	}
