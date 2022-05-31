7d47b183e414a68eb65dc14a7b4def960d45ee4d
zhongqi <imzhongqi@gmail.com>
2022-05-31 11:10:15 +0800

    feat: add command language_code.

--- command.go
+++ command.go
@@ -37,3 +37,54 @@ func (c CommandScope) toScope() tgbotapi.BotCommandScope {
 		UserID: c.UserID,
 	}
 }
+
+// NewCommandScopeDefault represents the default scope of bot commands.
+func NewCommandScopeDefault() CommandScope {
+	return CommandScope{Type: "default"}
+}
+
+// NewCommandScopeAllPrivateChats represents the scope of bot commands,
+// covering all private chats.
+func NewCommandScopeAllPrivateChats() CommandScope {
+	return CommandScope{Type: "all_private_chats"}
+}
+
+// NewCommandScopeAllGroupChats represents the scope of bot commands,
+// covering all group and supergroup chats.
+func NewCommandScopeAllGroupChats() CommandScope {
+	return CommandScope{Type: "all_group_chats"}
+}
+
+// NewCommandScopeAllChatAdministrators represents the scope of bot commands,
+// covering all group and supergroup chat administrators.
+func NewCommandScopeAllChatAdministrators() CommandScope {
+	return CommandScope{Type: "all_chat_administrators"}
+}
+
+// NewCommandScopeChat represents the scope of bot commands, covering a
+// specific chat.
+func NewCommandScopeChat(chatID int64) CommandScope {
+	return CommandScope{
+		Type:   "chat",
+		ChatID: chatID,
+	}
+}
+
+// NewCommandScopeChatAdministrators represents the scope of bot commands,
+// covering all administrators of a specific group or supergroup chat.
+func NewCommandScopeChatAdministrators(chatID int64) CommandScope {
+	return CommandScope{
+		Type:   "chat_administrators",
+		ChatID: chatID,
+	}
+}
+
+// NewCommandScopeChatMember represents the scope of bot commands, covering a
+// specific member of a group or supergroup chat.
+func NewCommandScopeChatMember(chatID, userID int64) CommandScope {
+	return CommandScope{
+		Type:   "chat_member",
+		ChatID: chatID,
+		UserID: userID,
+	}
+}
